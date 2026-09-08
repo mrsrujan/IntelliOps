d3deee7f58d2387bd4f4182d081ea06de36cef6c
Srujan <mr.srujan@gmail.com>
2026-09-08 12:33:12 -0500

    fix: also revert eks module version pin in main.tf (matches versions.tf)

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "~> 20.0"
 
   cluster_name    = "${var.project}-${var.environment}"
   cluster_version = "1.33"
